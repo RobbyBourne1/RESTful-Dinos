e2af8523166bd795df53d8ffeedec3beb3ec3397
Robby Bourne <robbybourne1@gmail.com>
2017-08-08 15:30:22 -0400

    Finished Daily

--- server.js
+++ server.js
@@ -10,29 +10,29 @@ app.set('view engine', 'mst')
 app.use(express.static('public'))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extend: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 let allDinos = [
   {
     id: 1,
     name: 'T-Rex',
-    color: ['Green'],
+    color: ['Green', 'Red'],
     diet: 'Carnivore'
   },
   {
-    id: 1,
+    id: 2,
     name: 'Raptor',
     color: ['Green', 'Purple'],
     diet: 'Carnivore'
   },
   {
-    id: 1,
+    id: 3,
     name: 'Stegosaurus',
     color: ['Green', 'Yellow'],
     diet: 'Herbivore'
   },
   {
-    id: 1,
+    id: 4,
     name: 'Triceratops',
     color: ['Orange', 'Yellow'],
     diet: 'Herbivore'
@@ -44,13 +44,45 @@ app.get('/api/dinos', (request, response) => {
 })
 
 app.get('/api/dinos/:id', (request, response) => {
-  const dinoId = paresInt(request.params.id)
+  const dinoId = parseInt(request.params.id)
   const thisDino = allDinos.find(dino => {
-    dino.id === dinoId
+    return dino.id === dinoId
   })
+  response.json(thisDino)
 })
 
-app.get('api/dinos/:id/diet', (request, response) => {})
+app.get('/api/dinos/:id/color', (request, response) => {
+  const dinoId = parseInt(request.params.id)
+  const thisDino = allDinos.find(dino => {
+    return dino.id === dinoId
+  })
+  response.json(thisDino.color)
+})
+
+app.post('/api/dinos', (request, response) => {
+  let newDino = {
+    id: allDinos.length + 1,
+    name: request.body.name,
+    color: request.body.color,
+    diet: request.body.diet
+  }
+  allDinos.push(newDino)
+  response.json(newDino)
+})
+
+app.put('/api/dinos/:id', (request, response) => {
+  const dinoId = parseInt(request.params.id)
+  if (dinoId !== allDinos.id) {
+    allDinos.push(dinoId)
+  }
+  response.json(allDinos)
+})
+
+app.delete('/api/dinos/:id', (request, response) => {
+  const dinoId = parseInt(request.params.id)
+  allDinos = allDinos.filter(dino => dino.id !== dinoId)
+  response.json(allDinos)
+})
 
 app.listen(3000, () => {
   console.log('Clever Girl')
